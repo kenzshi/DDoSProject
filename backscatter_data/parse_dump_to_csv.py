c63ce196e87e5302f85ec3b8dc30ffe2720d988a
Calvin <calvin.chan.h@gmail.com>
2015-03-16 00:22:42 -0700

    CSV

---
+++ backscatter_data/parse_dump_to_csv.py
@@ -0,0 +1,39 @@
+import re, json, csv
+
+f = open('data_large.dat', 'r')
+dump = f.read()
+p = re.compile(r'(\d{2}:\d{2}:\d{2})')
+dump_list = filter(None, p.split(dump))
+request_list = []
+
+for x in range(len(dump_list)):
+    if x%2 != 0:
+        continue
+    timestamp = dump_list[x]
+    tcpdump = dump_list[x+1]
+    lines = tcpdump.split('\n')
+    protocol = lines[0].split(' ')
+    if len(protocol) > 1:
+        if protocol[1] != 'IP':
+            continue
+        else:
+            request = re.compile(r'\s+').split(lines[1])
+            source = request[1]
+            dest = request[3]
+            request_dict = {'timestamp':timestamp, 'source':source, 'dest':dest}
+            if re.match('^client-lan0.+',source):
+                request_dict['type'] = 'NORMAL'
+            elif re.match('^server-lan0.+',source):
+                request_dict['type'] = 'RESPONSE'
+            else:
+                request_dict['type'] = 'POSSIBLE_DDOS'
+            request_list.append(request_dict)
+    else:
+        continue
+
+csv_file = open('data.csv', 'wb+')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['timestamp','type','source','dest'])
+for request in request_list:
+    csv_writer.writerow([request['timestamp'],request['type'],request['source'],request['dest']])
+csv_file.close()
